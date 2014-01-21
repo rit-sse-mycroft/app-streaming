6b37e9af0a6d8a74db51aaa9c95379bb1e5e3f1d
weswigham <wwigham@gmail.com>
2014-01-21 11:11:32 -0500

    UI updated to include targets field and have a background

--- streaming.rb
+++ streaming.rb
@@ -9,7 +9,13 @@ class Streaming < Mycroft::Client
     @cert = './streaming.crt'
     @manifest = './app.json'
     @verified = false
+    @host = 'localhost'
+    @players = {}
     super
+  end
+  
+  def reg_ui(ui)
+    @ui = ui
   end
 
   def connect
@@ -22,12 +28,16 @@ class Streaming < Mycroft::Client
 
   def on_data(parsed)
     if parsed[:type] == 'MSG_QUERY_SUCCESS'
-        puts "Stream started!"
+      puts "Stream started!"
+    end
+    if parsed[:type] == 'APP_DEPENDENCY'
+      puts "Updating player list"
+      @players = parsed[:data]['video']
+      @ui.players_changed(@players)
     end
   end
 
   def on_end
-    down
   end
 end
 
--- ui.rb
+++ ui.rb
@@ -5,22 +5,21 @@ require 'open-uri'
 require './streaming'
 
 class StreamURLMaker < Wx::Frame
-  def initialize(title, thread)
+  def initialize(title)
     super(nil, -1, title, nil, Wx::Size.new(340,600), Wx::DEFAULT_FRAME_STYLE ^ Wx::RESIZE_BORDER)
+    @bg = Wx::Panel.new(self)
     
-    @mycroft = thread
+    @sizer = Wx::GridBagSizer.new(4, 0)
     
-    @sizer = Wx::GridBagSizer.new()
-    
-    @reslabel = Wx::StaticText.new(self, -1, "Resolution Scale")
-    @resolution = Wx::SpinCtrl.new(self, -1, "", nil, nil, nil, 1, 100, 100)
-    @perlabel = Wx::StaticText.new(self, -1, "%")
+    @reslabel = Wx::StaticText.new(@bg, -1, "Resolution Scale")
+    @resolution = Wx::SpinCtrl.new(@bg, -1, "", nil, nil, nil, 1, 100, 100)
+    @perlabel = Wx::StaticText.new(@bg, -1, "%")
     @ressizer = Wx::BoxSizer.new(Wx::HORIZONTAL)
     @ressizer.add(@reslabel, 0, Wx::ALIGN_CENTRE | Wx::ALL, 4)
     @ressizer.add(@resolution, 1, Wx::EXPAND)
     @ressizer.add(@perlabel, 0, Wx::ALIGN_CENTRE | Wx::ALL, 4)
     
-    @filebutton = Wx::Button.new(self, -1, "File")
+    @filebutton = Wx::Button.new(@bg, -1, "File")
     evt_button(@filebutton) do #Get a file, make a vlc launch line form it, launch vlc, send our info to mycroft
       dialog = Wx::FileDialog.new(self, "Choose a file to stream")
       res = dialog.show_modal
@@ -28,22 +27,27 @@ class StreamURLMaker < Wx::Frame
         path = Pathname.new(dialog.get_directory+"\\\\"+dialog.get_filename).expand_path.to_s
         escaped = URI.escape(path, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
         url = "file:///"+escaped
+        res = @ressizer.get_value
+        frac = "%0.2f" % (res.to_i/100)
         
-        launch = "vlc #{url} :sout=#transcode{vcodec=h264,scale=1,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=t140,soverlay}:rtp{sdp=rtsp://:8080/mycroft.sdp} :sout-keep"
+        launch = "vlc #{url} :sout=#transcode{vcodec=h264,scale=#{frac},acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=t140,soverlay}:rtp{sdp=rtsp://:8080/mycroft.sdp} :sout-keep"
         pid = spawn(launch)
         sendIp
       end
     end
     
-    @screenbutton = Wx::Button.new(self, -1, "Screen")
+    @screenbutton = Wx::Button.new(@bg, -1, "Screen")
     evt_button(@screenbutton) do #Make vlc launch line, send info to mycroft
       url = "screen://"
-      launch = "vlc #{url} :screen-fps=30 :screen-caching=100 :sout=#transcode{vcodec=h264,scale=1,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=t140,soverlay}:rtp{sdp=rtsp://:8080/mycroft.sdp} :sout-keep"
+      res = @ressizer.get_value
+      frac = "%0.2f" % (res.to_i/100)
+    
+    launch = "vlc #{url} :screen-fps=30 :screen-caching=100 :sout=#transcode{vcodec=h264,scale=#{frac},acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=t140,soverlay}:rtp{sdp=rtsp://:8080/mycroft.sdp} :sout-keep"
       pid = spawn(launch)
       sendIp
     end
     
-    @youtubebutton = Wx::Button.new(self, -1, "Youtube")
+    @youtubebutton = Wx::Button.new(@bg, -1, "Youtube")
     evt_button(@youtubebutton) do #Send url to mycroft
       dialog = Wx::TextEntryDialog.new(self, "Youtube video URL")
       res = dialog.show_modal
@@ -58,8 +62,18 @@ class StreamURLMaker < Wx::Frame
     @sizer.add(@youtubebutton, Wx::GBPosition.new(0,2))
     
     @sizer.add(@ressizer, Wx::GBPosition.new(1,0), Wx::GBSpan.new(1, 3))
+    @checksizer = Wx::StaticBoxSizer.new(Wx::VERTICAL, @bg, "Target Players")
     
-    set_sizer_and_fit @sizer
+    
+    @sizer.add(@checksizer, Wx::GBPosition.new(2,0), Wx::GBSpan.new(1, 3), Wx::EXPAND)
+    
+    @bg.set_sizer_and_fit @sizer
+    
+    bgsizer = Wx::BoxSizer.new(Wx::VERTICAL)
+    bgsizer.add(@sizer)
+    
+    set_sizer_and_fit bgsizer
+    
     show
   end
   
@@ -72,16 +86,48 @@ class StreamURLMaker < Wx::Frame
   end
 
   def sendUrl(url)
-    Streaming.sendUrl(url)
+    dest = []
+    @checksizer.get_children.each do |obj|
+      if (obj.is_a? Wx::CheckBox) and obj.is_checked
+        dest.add(obj.get_label_text)
+      end
+    end
+    
+    #Streaming.sendUrl(url, dest)
+  end
+  
+  def update_players(players)
+    @checksizer.get_children.each do |obj|
+      if obj.is_a? Wx::CheckBox
+        obj.remove
+      end
+    end
+    
+    players.each do |key, value|
+      if (value!='down')
+        box = Wx::CheckBox.new(@bg, -1, key.to_s)
+        box.set_value true
+        @checksizer.add(box, 1, Wx::EXPAND | Wx::CENTER)
+      end
+    end
+    
+    @sizer.fit self
+    @sizer.fit @bg
   end
   
 end
 
 
 class MycroftStreamUI < Wx::App
-  Mycroft.start(Streaming) # This is blocking. Why is this blocking.
-
-  @frame = StreamURLMaker.new("Video Stream Launcher", thr)
+  #Mycroft.start(Streaming) # This is blocking. Why is this blocking.
+  def on_init
+    @frame = StreamURLMaker.new("Video Stream Launcher")
+    # Streaming.reg_ui(self)
+  end
+  
+  def players_changed(players)
+    @frame.update_players(players)
+  end
 end
 
 app = MycroftStreamUI.new
